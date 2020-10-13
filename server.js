68788d94fbd8dc00a1199e5f5008b5ef3b090d68
Jennifer Schlierman <jd9913@gmail.com>
2020-10-12 18:51:44 -0700

    connected npm to database

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const sqlite3 = require('sqlite3').verbose();
+
 const express=require('express');
 
 const PORT = process.env.PORT||3001;
@@ -5,6 +7,14 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+// Connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+      return console.error(err.message);
+    }
+  
+    console.log('Connected to the election database.');
+  });
 
 
 
@@ -15,8 +25,10 @@ app.use(express.json());
 
 
 
-
-
+// Default response for any other requests(Not Found) Catch all
+app.use((req, res) => {
+    res.status(404).end();
+  });
 
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`);
